59a9123125d09d7b2409657b8f42c710b2296d07
CHARL <charlesdev48@gmail.com>
2018-03-21 18:34:08 +0300

    configured the database settings

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'crmapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crmeasyDB',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5000',
     }
 }
 
