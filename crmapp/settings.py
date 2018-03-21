9a3d7dab3c777c0d873a94fd401da721d13c08a3
CHARL <charlesdev48@gmail.com>
2018-03-21 19:26:53 +0300

    configured static file settings

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -108,4 +108,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+# STATIC_URL = '/static/'
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
